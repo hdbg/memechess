34aa9c89d06735f130daf04e75b486af15b4e5ca
hdbg <hdbg@gmail.com>
2022-04-02 00:07:28 +0300

    Якимось, блять, просто магічним чином зробив клятий маленький термінальчик

--- src/client/api.nim
+++ src/client/api.nim
@@ -4,8 +4,14 @@ type Object* = ref object of JsRoot
 var
   ObjectManager* {.importc: "Object", nodecl.}: Object
   window* {.importc, nodecl.}: JsObject
+  jQuery*{.importc, nodecl.}: JsObject
+
+
 
 proc defineProperty*(mng: Object, target: JsObject, property: cstring,
     value: auto) {.importjs: "#.defineProperty(@)", nodecl.}
+
+
+proc jq*(callback: proc(d: proc(s: cstring): JsObject, undefined: JsObject)) {.importjs: "jQuery(#)".}
 
 # proc convert(x: JsObject, t: typedesc) =
