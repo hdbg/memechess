a2b48e2be90c1b42f19386a9484c42088da025cc
hdbg <90869971+hdbg@users.noreply.github.com>
2022-04-07 01:20:36 +0300

    Додав функції парсінга та серіалізації

--- src/chess/uci.nim
+++ src/chess/uci.nim
@@ -1,4 +1,4 @@
-import std/[strutils, options, pegs, parseutils]
+import std/[strutils, options, parseutils, tables, pegs, strformat]
 
 type
   EngineMessageKind* = enum
@@ -75,16 +75,23 @@ proc get(name, msg: string, target: var uint) {.inline.} =
 proc get(name, msg: string, target: var float) {.inline.} =
   parseFloat(msg, target, start=skipIdent(name, msg))
 
-proc get(name, msg: string, target: var string) {.inline.} =
+proc get(name, msg: string, target: var bool) {.inline.} =
+  parseBool(msg, target, start=skipIdent(name, msg))
+
+proc get(name, msg: string, target: var string, space: bool = false) {.inline.} =
   let identEnd = skipIdent(name, msg)
-  parseUntil(msg, target, '\n')
+
+  var delims = {'\n'}
+  if space: delims.incl ' '
+
+  parseUntil(msg, target, delims)
 
 proc get[T](name, msg: string, target: var Option[T]) {.inline.} =
   var val: T
   get(name, msg, val)
   target = some(val)
 
-# Low-level api
+# Engine-to-gui
 proc parseInfo(msg: string): EngineMessage =
   result = EngineMessage(kind: emkInfo)
 
@@ -110,5 +117,142 @@ proc parseInfo(msg: string): EngineMessage =
 
   info.pv = rawPv.split()
 
+  result.info = move(info)
+
+proc parseOption(msg: string): EngineMessage =
+  result = EngineMessage(kind: emkOption)
+
+  var option: EngineOption
+
+  get "name", msg, option.name, true
+
+  var oType: string
+  get "type", msg, oType, true
+
+  const strToKind = {"check": eokCheck,
+                     "spin": eokSpin,
+                     "combo": eokCombo,
+                     "button": eokButton,
+                     "string": eokString
+                    }.toTable
+
+  option.kind = strToKind[oType]
+
+  case option.kind
+  of eokCheck:
+    get "default", msg, option.check
+  of eokSpin:
+    get "default", msg, option.spin
+    get "min", msg, option.min
+    get "max", msg, option.max
+  of eokString:
+    get "default", msg, option.str
+  of eokCombo:
+    get "default", msg, option.combo, true
+    const varSearch = peg"var {\w+}"
+    match(msg, varSearch, option.values)
+  else: discard
+
+  result.option = move(option)
 
 proc getMessage*(msg: string): EngineMessage = discard
+
+# Gui-to-engine
+
+type
+  GuiMessageKind = enum
+    gmkUci
+    gmkDebug
+    gmkIsReady
+    gmkSetOption
+    gmkRegister
+    gmkUciNewGame
+    gmkPosition
+    gmkGo
+    gmkStop
+    gmkPonderHit
+    gmkQuit
+
+  GuiMessage = object
+    case kind: GuiMessageKind
+    of gmkDebug:
+      debug: bool
+    of gmkSetOption:
+      name: string
+      value: Option[string]
+    of gmkPosition:
+      fen: Option[string]
+      moves: seq[string]
+    of gmkGo:
+      searchmoves: seq[string]
+      wtime, btime: Option[float]
+      winc, binc: Option[uint]
+      movestogo: uint # No option cause counts only if > 0
+
+      depth, nodes, mate: Option[uint]
+      movetime: Option[uint]
+
+      # Add infinite switch here
+    else: discard
+
+proc `$`*(msg: GuiMessage): string =
+  result = ""
+
+  case msg.kind
+  of gmkDebug:
+    result = "debug " & $msg.debug
+  of gmkSetOption:
+    result = "setoption name " & msg.name
+    if value.isSome:
+      result.add " value " & msg.value.get
+
+  of gmkPosition:
+    result = "position "
+
+    if msg.fen.isSome:
+      result.add " fen " & msg.fen.get
+    else:
+      result.add " startpos"
+
+    if unlikely(msg.moves.len > 0):
+      result.add " moves"           # no whitespace in the end, because every added move adds it
+      for move in msg.moves:
+        result.add &" {move} "
+  of gmkGo:
+    template optionInsert(name: untyped, uciName: Option[string]) =
+      let
+        value = $msg.name
+        realName = if uciName.isSome: uciName.get else: &"{name}" # sorry for COSTYL
+
+      result.add &" {realName} {value}"
+
+    # im sorry, this is ugly
+    optionInsert wtime
+    optionInsert btime
+    optionInsert winc
+    optionInsert binc
+    optionInsert movestogo
+    optionInsert depth
+    optionInsert nodes
+    optionInsert mate
+    optionInsert movetime
+
+    if msg.searchmoves.len > 0:
+      result,add " searchmoves"
+
+      for mov in searchmoves:
+        result.add " " & move
+
+when false:
+  proc uci(): string = "uci"
+  proc debug(mode: bool): string = "debug " & if mode: "on" else: "off"
+  proc isReady(): string = "isready"
+  proc setOption(name: string, value: Option[string]): string =
+    if value.isSome:
+      return &"setoption name {name} value {value}"
+    else:
+      return "setoption name " & value
+
+  # proc register
+
+  proc uciNewGame(): string = "ucinewgame"
